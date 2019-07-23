e1a793ebe745eb3a54d9ce4d022885f8f2d87f14
cosineta <18710960952@yeah.net>
2019-07-23 14:10:05 +0800

    c

---
+++ Member/YSY/ACM/0718/c.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,sum,a[10];
+	scanf("%d",&n);
+	
+	sum=0;
+	for(i=1;i<=n;i++)
+	{	scanf("%d",&a[i]);
+		sum+=a[i];	
+	}
+	
+	for(i=1;i<=n;i++)
+	{
+		sum-=(n+1-i);
+		if(sum<=0)
+			break;
+			
+	}
+
+	printf("%d",i);
+
+	return 0;
+}
